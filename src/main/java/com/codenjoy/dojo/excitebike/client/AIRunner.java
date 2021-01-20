1e9c6001171ddccdc3ad980d392dcde00ccf4924
Oleksandr_Baglai <apofig@gmail.com>
2021-01-20 16:46:06 +0200

    [games] Теперь есть возможность задавать в параметрах main класса URL и он будет перекрывать захардкодженый в коде

--- src/main/java/com/codenjoy/dojo/excitebike/client/AIRunner.java
+++ src/main/java/com/codenjoy/dojo/excitebike/client/AIRunner.java
@@ -42,10 +42,10 @@ public class AIRunner implements Solver<Board> {
     }
 
     public static void main(String[] args) {
-        // paste here board page url from browser after registration
-        String url = "http://codenjoy.com:80/codenjoy-contest/board/player/3edq63tw0bq4w4iem7nb?code=1234567890123456789";
-        WebSocketRunner.runClient(
-                url,
+        WebSocketRunner.runClient(args,
+                // paste here board page url from browser after registration
+                // or put it as command line parameter
+                "http://codenjoy.com:80/codenjoy-contest/board/player/3edq63tw0bq4w4iem7nb?code=1234567890123456789",
                 new AIRunner(new RandomDice()),
                 new Board());
     }
